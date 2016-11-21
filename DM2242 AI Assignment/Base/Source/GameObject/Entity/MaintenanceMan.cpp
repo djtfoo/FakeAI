fe91699757cb09c7ba03c720e95a4e16cc469b6a
TemplarDon <donovandenzel@gmail.com>
2016-11-22 01:02:13 +0800

    Basic MaintenanceMan fsm

--- DM2242 AI Assignment/Base/Source/GameObject/Entity/MaintenanceMan.cpp
+++ DM2242 AI Assignment/Base/Source/GameObject/Entity/MaintenanceMan.cpp
@@ -1,5 +1,7 @@
 #include "MaintenanceMan.h"
 
+#include "../../SharedData.h"
+
 MaintenanceMan::MaintenanceMan() : Entity("Maintenance Man")
 {
 }
@@ -10,22 +12,60 @@ MaintenanceMan::~MaintenanceMan()
 
 void MaintenanceMan::Init()
 {
-
+    m_breakCharge = 0;
+    m_timer = 0;
+    m_state = IDLE;
+    m_targetMachine = NULL;
+    m_doingWork = false;
 }
 
 void MaintenanceMan::Update(double dt)
 {
+    if (m_targetMachine)
+    {
+        if (!m_doingWork)
+        {
+            Vector3 dir = (m_targetMachine->GetPos() - m_pos).Normalized();
+            m_pos += dir * dt;
+        }
 
+        if ((m_pos - m_targetMachine->GetPos()).Length() < 1.001)
+        {
+            m_doingWork = true;
+        }
+    }
 }
 
 void MaintenanceMan::Sense(double dt)
 {
+    m_breakCharge += dt * Math::RandFloatMinMax(1, 2);
 
+    if ((m_state == REFILL || m_state == REPAIR) && m_doingWork)
+        m_timer += dt;
+    else if (m_state == REPAIR)
+        m_timer += dt;
 }
 
 int MaintenanceMan::Think()
 {
+ 
+    switch (m_state)
+    {
 
+    case IDLE:
+        return ScanMachines();
+        break;
+        
+    case REFILL:
+        break;
+
+    case REPAIR:
+        break;
+
+    case BREAK:
+        break;
+
+    }
 
     return 0;
 }
@@ -34,10 +74,77 @@ void MaintenanceMan::Act(int value)
 {
     switch (value)
     {
-    case 0:
+    case IDLE:
+        DoIdle();
         break;
 
-    case 1:
+    case REPAIR:
+        DoRepair();
+        break;
+
+    case REFILL:
+        DoRefill();
+        break;
+
+    case BREAK:
+        DoBreak();
         break;
     }
+}
+
+int MaintenanceMan::ScanMachines()
+{
+    for (int i = 0; i < SharedData::GetInstance()->m_goList.size(); ++i)
+    {
+        GameObject* go = dynamic_cast<GameObject*>(SharedData::GetInstance()->m_goList[i]);
+
+        if (!go->IsActive())
+            continue;
+
+        if (go->GetName() == "Machine")
+        {
+            Machine* machine = dynamic_cast<Machine*>(go);
+
+            if (machine->GetState() == Machine::BROKEN)
+            {
+                m_targetMachine = machine;
+                return REPAIR;
+            }
+            else if (machine->GetState() == Machine::WAITFORREFILL)
+            {
+                m_targetMachine = machine;
+                return REFILL;
+            }
+        }
+    }
+
+    return IDLE;
+}
+
+void MaintenanceMan::DoIdle()
+{
+
+}
+
+void MaintenanceMan::DoRepair()
+{
+    if (m_timer > 4)
+    {
+        m_targetMachine->SetIsBroken(false);
+        m_timer = 0;
+    }
+}
+
+void MaintenanceMan::DoRefill()
+{
+    if (m_timer > 4)
+    {
+        m_targetMachine->SetIsEmpty(false);
+        m_timer = 0;
+    }
+}
+
+void MaintenanceMan::DoBreak()
+{
+
 }
--- DM2242 AI Assignment/Base/Source/GameObject/Entity/MaintenanceMan.h
+++ DM2242 AI Assignment/Base/Source/GameObject/Entity/MaintenanceMan.h
@@ -2,10 +2,12 @@
 #define MAINTENANCE_MAN_H
 
 #include "Entity.h"
+#include "Machine.h"
 
 class MaintenanceMan : public Entity
 {
 public:
+  
     enum MAINTENANCEMAN_STATE
     {
         IDLE,
@@ -15,9 +17,20 @@ public:
     };
 
 private:
-    double d_breakCharge;
+    double m_breakCharge;
+    double m_timer;
 
     MAINTENANCEMAN_STATE m_state;
+
+    Machine* m_targetMachine;
+
+    bool m_doingWork;
+
+    int ScanMachines();
+    void DoIdle();
+    void DoRepair();
+    void DoRefill();
+    void DoBreak();
 
     virtual void Sense(double dt);
     virtual int Think();
